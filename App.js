4dd2044366e4f867c3315c7ca9d85fde41d27f4b
sahanudara25 <sahanherath444@gmail.com>
2023-07-16 16:57:34 +0530

    Created Login Button -Sahan

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text>This is the 1st Screen</Text>
+            </View>
+            <View>
+                <Button title={"Login"}  color={"red"} disabled={false}/>
+            </View>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        flexDirection:"row"
+    },
 });
---
+++ components/LoginButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+
+const LoginButton=(title)=>{
+
+
+    return(
+
+        <View>
+            <Text>
+                {title}
+            </Text>
+        </View>
+    )
+}
